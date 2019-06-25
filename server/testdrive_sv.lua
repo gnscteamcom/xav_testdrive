489574b8dbff9954fb0c29439d4798f1bcd252ab
xavlios <5066808+xavlios@users.noreply.github.com>
2019-06-25 12:53:58 -0400

    Create testdrive_sv.lua

---
+++ server/testdrive_sv.lua
@@ -0,0 +1,14 @@
+SX              = nil
+local Categories = {}
+local Vehicles   = {}
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+
+ESX.RegisterServerCallback('esx_bmvehicles:getCategories', function (source, cb)
+	cb(Categories)
+end)
+
+ESX.RegisterServerCallback('esx_bmvehicles:getVehicles', function (source, cb)
+	cb(Vehicles)
+end)
